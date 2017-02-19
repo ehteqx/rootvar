91a9efd6349a846ed6a063a83633a0a0ab5a0eda
Emanuele Ballarin <emanuele@ballarin.cc>
2017-02-19 19:29:14 +0100

    Some test (still, correct) modifications

--- rui-root/Exercise4.C
+++ rui-root/Exercise4.C
@@ -1,21 +1,22 @@
 /*Write a named script that asks the user to type 10 integers
 and writes the smallest value.*/
 
-#include "Riostream.h" 
+#include "Riostream.h"
+using namespace std;
 
-void Exercise4() {  
+void Exercise4() {
 
   Int_t a = 0;
   Int_t b = 0;
-  
+
   //for LOOP
 
   for (Int_t i = 0 ; i < 10 ; i++) {
 
-    cout << " i " << i << " a : " ;
-    cin >> a ;
-    if ( i == 0 ) b = a;
-    if ( a < b ) b = a;
+	cout << " i " << i << " a : " ;
+	cin >> a ;
+	if ( i == 0 ) b = a;
+	if ( a < b ) b = a;
 
   }
 
@@ -24,7 +25,7 @@ void Exercise4() {
 
   // Int_t i = 0;
   // while ( i < 10 ) {
-    
+
   //   cout << " i " << i << " a : " ;
   //   cin >> a ;
   //   if ( i == 0 ) b = a;
--- warm-up/cube.C
+++ warm-up/cube.C
@@ -1,6 +1,11 @@
 // ROOT 6 using C++11
 // This macro asks for a number and calculates its cube
 
+#import "TMath.h"
+#import "Riostream.h"
+
+using namespace std;
+
 void cube() {
 
 	cout << "Please, insert a number... " << endl;
